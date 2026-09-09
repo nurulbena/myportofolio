e36046e0eca3c4721b42dec35f56e4c446458088
Nurul Fikryati Bena <nurulbena13@gmail.com>
2026-09-09 20:04:02 +0700

    Tutorial 2: implementasi MVT pada Django dan unit test

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Nurul Fikryati Bena",
+        "npm": "2506534825",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Iformation Systems student at Universitas Indonesia with a strong interest in programming, digital technology and design."
+            "Experienced in working both independently and collaboratively in team environments."
+            "Highly motivated to continue learning and developing skills in technology, visual design, and multimedia creation."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Nurul Fikryati Bena",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
